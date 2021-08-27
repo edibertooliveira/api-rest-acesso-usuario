9d59d27c3f150f41091637209b6b7887586f747f
Ediberto Oliveira <edibertooliveira@aol.com>
2021-08-27 12:24:46 -0300

    refactored name

---
+++ src/__test__/mocks/usersMake.ts
@@ -0,0 +1,41 @@
+export const password =
+  '$2a$08$JST6i/.s2uKZbgefdgz.U.fqneH6DFZZqjGrC3Gm03a7lcoJ.kamK';
+
+export const users = [
+  {
+    name: 'user 1',
+    email: 'user1@user.com',
+    password,
+    role: 'customer',
+  },
+  {
+    name: 'user 2',
+    email: 'user2@user.com',
+    password,
+    role: 'customer',
+  },
+  {
+    name: 'user 3',
+    email: 'user3@user.com',
+    password,
+    role: 'customer',
+  },
+  {
+    name: 'user 4',
+    email: 'user4@user.com',
+    password,
+    role: 'customer',
+  },
+  {
+    name: 'user 5',
+    email: 'user5@user.com',
+    password,
+    role: 'customer',
+  },
+  {
+    name: 'user 6',
+    email: 'user6@user.com',
+    password,
+    role: 'customer',
+  },
+];
